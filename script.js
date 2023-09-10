63c41ee6aa5bc4914276b39f9fbd5b04fc8df7c7
Prathamesh Palkar <prathameshpalkar93@gmail.com>
2023-09-10 21:07:16 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+function clearAll() {
+  document.getElementById("screenInp").value = "";
+}
+
+function clearOne() {
+  let currentVal = document.getElementById("screenInp").value;
+  currentVal = currentVal.slice(0, -1);
+  document.getElementById("screenInp").value = currentVal;
+}
+
+function calculateResult() {
+  try {
+    let expression = document.getElementById("screenInp").value;
+    expression = expression.replace("%", "/100");
+    expression = expression.replace("^", "**");
+
+    if (expression.includes("!")) {
+      const operand = parseFloat(expression.split("!")[0]);
+      let result = 1;
+      for (let i = 2; i <= operand; i++) {
+        result *= i;
+      }
+      expression = result.toString();
+    } else if (expression.includes("√")) {
+      const operand = parseFloat(expression.split("√")[1]);
+      expression = Math.sqrt(operand).toString();
+    } else if (expression.includes("log")) {
+      const operand = parseFloat(expression.split("log")[1]);
+      expression = Math.log10(operand).toString();
+    } else if (expression.includes("ln")) {
+      const operand = parseFloat(expression.split("ln")[1]);
+      expression = Math.log(operand).toString();
+    }
+
+    document.getElementById("screenInp").value = eval(expression);
+  } catch (error) {
+    document.getElementById("screenInp").value = "Error";
+  }
+}
